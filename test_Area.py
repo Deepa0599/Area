9d71021c60b8fb354b16b96dc8470d78b9a7319c
Deepa0599 <gadhavedeepa07@gmail.com>
2022-03-28 16:39:16 +0530

    Passed

--- test_Area.py
+++ test_Area.py
@@ -1,20 +1,21 @@
 import Area
+import pytest
 
-
+@pytest.mark.skip(reason = "currently no need to execute this")
 def test_area():
     a = 20
     b = 5
     r = Area.area(a, b)
     assert a*b == r
 
-
+@pytest.mark.myproject
 def test_perimeter():
     a = 10
     b = 5
     r = Area.perimeter(a, b)
     assert 2*(a+b) == r
 
-
+@pytest.mark.myproject
 def test_square():
     b = 5
     r = Area.square(b)
